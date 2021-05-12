4e84286ef269bae6da7ee4631fd7ba7d1c9a9396
matt <matt17@qq.com>
2021-05-12 19:00:34 +0800

    添加open feign, hystrix

---
+++ cloud-provider-hystrix-payment8001/src/main/java/com/matt/springcloud/service/PaymentService.java
@@ -0,0 +1,42 @@
+package com.matt.springcloud.service;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author matt
+ * @create 2021-05-12 14:27
+ */
+@Service
+public class PaymentService {
+
+    //成功
+    public String paymentInfo_OK(Integer id) {
+        return "线程池：" + Thread.currentThread().getName() + "   paymentInfo_OK,id：  " + id + "\t" + "哈哈哈";
+    }
+
+    // 失败
+    @HystrixCommand(fallbackMethod = "paymentInfo_TimeOutHandler", commandProperties = {
+            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "3000")
+    })
+    public String paymentInfo_TimeOut(Integer id) {
+        int timeNumber = 5;
+        try {
+            TimeUnit.SECONDS.sleep(timeNumber);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "线程池：" + Thread.currentThread().getName() + "   paymentInfo_TimeOut,id：  " + id + "\t" + "呜呜呜" + "耗时(秒)" + timeNumber;
+    }
+
+
+    public String paymentInfo_TimeOutHandler(Integer id) {
+
+        return "线程池：" + Thread.currentThread().getName() + "paymentInfo_TimeOutHandler,id：  " + id + "\t" + "didi";
+    }
+
+
+}
